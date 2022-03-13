9196408ef394b1a02923612ab4027da379183e0d
Arda <ardahadimioglu@gmail.com>
2022-03-13 23:20:12 +0100

    adds react router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,18 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { DashboardPage } from "../components/pages/Dashboard.page";
 import { HomePage } from "../components/pages/Home.page";
 import { Footer } from "../components/single-types/Footer";
 import { Header } from "../components/single-types/Header";
-import { usePollContract } from "../hooks/usePollContract";
-
 declare const window: { ethereum: any };
 
 const IndexPage = () => {
-  const { addUser, allUsers, isWalletConnected, addQuiz, allQuizes } =
-    usePollContract();
   return (
     <main className="flex h-screen flex-col justify-between">
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route
-            path="/dashboard"
-            element={<div className="bg-lime-200">arda</div>}
-          />
+          <Route path="/dashboard" element={<DashboardPage />} />
           <Route
             path="*"
             element={
@@ -36,7 +30,15 @@ const IndexPage = () => {
 
 export default IndexPage;
 
+// user already exists erroru aynen
+
+// todo grid yer ac contact form
+// todo same username issue
+// todo grid item lerin section style paddinglere dikkat after: ile sag uste tooltip gibi
+// todo quiz card style
 // todo contract hook bitir
+// todo event emit
 // todo animation
 // todo checkbox icine svg
-// todo dashboard
+// todo usercard ui improve
+// todo wait metamask confirm
