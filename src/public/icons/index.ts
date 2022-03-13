660a200665f4b2405905bae3f5dede9cb81a257e
Arda <ardahadimioglu@gmail.com>
2022-03-13 23:19:02 +0100

    adds icons

--- src/public/icons/index.ts
+++ src/public/icons/index.ts
@@ -2,3 +2,5 @@ export * from "./Logo.icon";
 export * from "./Dashboard.icon";
 export * from "./Burger.icon";
 export * from "./Home.icon";
+export * from "./Vote.icon";
+export * from "./Question.icon";
