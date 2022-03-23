7ad190cf31c13e55b9aed93ec897dcf2953e48f9
Arda <ardahadimioglu@gmail.com>
2022-03-23 17:15:44 +0100

    changes grid layout

--- src/components/pages/Dashboard.page.tsx
+++ src/components/pages/Dashboard.page.tsx
@@ -7,14 +7,14 @@ import { UsersCard } from "../main/users/UsersCard.component";
 export const DashboardPage = () => {
   return (
     <section className="dashboard-wrapper">
-      <div className="dashboard-card grid w-full grid-cols-2 grid-rows-6 lg:grid-cols-5">
-        <div className="dashboard-item col-span-2 row-span-4 lg:col-span-3 lg:row-span-6">
+      <div className="dashboard-card grid w-full grid-cols-2 grid-rows-6  md:grid-cols-5">
+        <div className="dashboard-item col-span-2 row-span-4 md:col-span-3 md:row-span-6 lg:col-span-3">
           <PollsCard />
         </div>
-        <div className="dashboard-item col-span-2 row-span-2 md:col-span-1 md:row-span-3 lg:col-span-2">
+        <div className="dashboard-item col-span-2 row-span-2 md:col-span-2 md:row-span-3 lg:col-span-2">
           <UsersCard />
         </div>
-        <div className="dashboard-item col-span-2 row-span-1 hidden md:col-span-1 md:row-span-3 md:block lg:col-span-2">
+        <div className="dashboard-item col-span-2 row-span-1 hidden md:col-span-2 md:row-span-3 md:block lg:col-span-2">
           <ContactForm />
         </div>
       </div>
